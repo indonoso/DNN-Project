cd385aff25e896f0437219b2f10882a4926a4621
Ivania Donoso <ivania.donoso@gmail.com>
2021-07-17 02:24:12 -0400

    Updated model to adjust to MatchLSTM

--- model.py
+++ model.py
@@ -3,19 +3,25 @@ from machine_comprehension.models import MatchLSTM
 
 
 class MatchLSTMModified(torch.nn.Module):
-    def __init__(self, network_input_size, match_lstm_input_size, word_embedding=False, part_of_speech=False, knowledge_graph=False):
+    def __init__(self, network_input_size, match_lstm_input_size, word_embedding=False, part_of_speech=False, knowledge_graph=False, hidden_size=150):
         super().__init__()
+        self.embedding = Embedding(word_embedding=word_embedding,
+                                   part_of_speech=part_of_speech,
+                                   knowledge_graph=knowledge_graph)
         self.reduce_embedding = torch.nn.Linear(network_input_size, match_lstm_input_size)
-        self.match_lstm = MatchLSTM(match_lstm_input_size)
-        self.embedding = Embedding(word_embedding=word_embedding, part_of_speech=part_of_speech, knowledge_graph=knowledge_graph)
+        self.match_lstm = MatchLSTM(embedding_size=match_lstm_input_size, hidden_size=hidden_size)
 
+    def forward(self, word_context, word_question, kg_context, kg_question, pos_context, pos_question,
+                context_lengths, question_lengths):
 
-    def forward(self, word_context, word_question, kg_context, kg_question, pos_context, pos_question, context_lengths, question_lengths):
+        context_vec, question_vec = self.embedding(word_context, word_question, kg_context, kg_question,
+                                                   pos_context, pos_question)
 
-        # Este reduce hay que modificarlo seguro.
-        # Hay que hacer padding y esas cosas para que se aplique la reducción a cada palabra, no a la frase completa.
-        context_vec, question_vec = self.embedding(word_context, word_question, kg_context, kg_question, pos_context, pos_question)
+        context_vec = self.reduce_embedding(context_vec)
+        question_vec = self.reduce_embedding(question_vec)
 
+        question_vec = torch.transpose(question_vec, 0, 1)
+        context_vec = torch.transpose(context_vec, 0, 1)
         return self.match_lstm(context_vec, context_lengths, question_vec, question_lengths)
 
 
@@ -25,18 +31,17 @@ class Embedding(torch.nn.Module):
         self.embedding = {}
 
         if isinstance(word_embedding, torch.Tensor):
-            self.embedding['word'] = torch.nn.Embedding.from_pretrained(word_embedding)
+            self.embedding['word'] = torch.nn.Embedding.from_pretrained(word_embedding, padding_idx=0)
         if isinstance(part_of_speech, torch.Tensor):
-            self.embedding['pos'] = torch.nn.Embedding.from_pretrained(part_of_speech)
+            self.embedding['pos'] = torch.nn.Embedding.from_pretrained(part_of_speech, padding_idx=0)
         if isinstance(knowledge_graph, torch.Tensor):
-            self.embedding['kg'] = torch.nn.Embedding.from_pretrained(knowledge_graph)
+            self.embedding['kg'] = torch.nn.Embedding.from_pretrained(knowledge_graph, padding_idx=0)
 
     def forward(self, word_context, word_question, kg_context, kg_question, pos_context, pos_question):
         context = self.apply_embedding(word_context, pos_context, kg_context)
         question = self.apply_embedding(word_question, pos_question, kg_question)
 
-        # TODO check que el cat esté en la dimensión correcta
-        return torch.cat(context, dim=0), torch.cat(question, dim=0)
+        return torch.cat(context, dim=2), torch.cat(question, dim=2)
 
     def apply_embedding(self, word, pos, kg):
         applied_embeddings = []
@@ -46,4 +51,5 @@ class Embedding(torch.nn.Module):
             applied_embeddings.append(self.embedding['pos'](pos))
         if 'kg' in self.embedding:
             applied_embeddings.append(self.embedding['kg'](kg))
+
         return applied_embeddings
--- utils.py
+++ utils.py
@@ -1,3 +1,20 @@
-def load_embeddings(kind, embedding_size):
-    # TODO
-    return # Tensors
+import pickle
+from torch import Tensor
+
+def get_input_size(kind, config):
+    network_input_size = 0
+    if config[f'use_{kind}']:
+        network_input_size = config[f'{kind}_embedding_size']
+    return network_input_size
+
+
+def get_embeddings_path(kind, config):
+    name = config['data']['processed'][f'{kind}_embeddings_path']
+    size = config['preprocess'][f'{kind}_embedding_size']
+    return f'{name}-{size}.pickle'
+
+
+def load_embeddings(path):
+    with open(path, 'rb') as f:
+        embeddings = pickle.load(f)
+    return Tensor(embeddings)
